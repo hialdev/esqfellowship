59dc6596a834900d3a821b9e7b173c0430f86aa0
hialdev <al.developer12@gmail.com>
2023-06-25 03:35:44 +0700

    Bismillah Laptop baru

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,19 @@ humburger.on('click',function(){
         menu.removeClass('active');
     }
 })
+
+// Modal About
+$('#contribute-modal').hide();
+$('#benefit-modal').hide();
+$('#close-modal').click(function(){
+    $('#contribute-modal').hide();
+})
+$('#benefit-close-modal').click(function(){
+    $('#benefit-modal').hide();
+})
+$('#contribute').on('click', function () {
+    $('#contribute-modal').show();
+})
+$('#benefit').on('click', function () {
+    $('#benefit-modal').show();
+})

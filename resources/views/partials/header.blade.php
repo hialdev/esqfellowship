dea5ab2c60d9fca601d8eb39a80060de08893a6c
hialdev <al.developer12@gmail.com>
2023-10-17 11:10:00 +0700

    logo by setting

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="d-flex align-items-center gap-4 menu p-3 px-3 px-lg-5 bg-transparent position-absolute top-0 left-0 right-0 w-100" style="z-index: 100;">
         <a href="{{route('home')}}" class="d-flex align-items-center justify-content-center text-decoration-none">
-            <img src="{{ request()->is('/') ? '/src/images/logo.png' : '/src/images/logo1.png'}}" alt="Logo ESQ Business School" style="max-height: 3em; filter: grayscale(100)">
+            <img src="{{ request()->is('/') ? '/src/images/logo.png' : Voyager::image(setting('site.logo'))}}" alt="Logo ESQ Business School" style="max-height: 3em; filter: grayscale(100)">
         </a>
         <div class="menu-list">
             <a href="{{route('about')}}" class="menu-item {{ request()->is('about') ? 'active' : ''}}">About</a>
